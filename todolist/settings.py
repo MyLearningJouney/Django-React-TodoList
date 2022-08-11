c0fe0da8d5599b8003ae8f48ce3b6443e1deb5dc
Danilo Packer <danilo.kpacker@gmail.com>
2022-08-11 14:35:28 +0000

    Remove a hardcoded Django secret key

--- todolist/settings.py
+++ todolist/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-%a45&pq@__d&z9xni@si&@)=owr6v--^m+h7l#w9qndsfw2!6a"
+#SECRET_KEY = "django-insecure-%a45&pq@__d&z9xni@si&@)=owr6v--^m+h7l#w9qndsfw2!6a"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
