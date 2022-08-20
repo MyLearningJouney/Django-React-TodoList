b242e2c9e1ff442e67e3f65a118088811eff3d89
Danilo Packer <danilo.kpacker@gmail.com>
2022-08-20 11:31:16 -0300

    Login Function

--- todoapp/api/urls.py
+++ todoapp/api/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('todoactivities/', views.TodoListCreate.as_view()),
     path('todoactivities/<int:pk>', views.TodoRetrieveUpdateDestroy.as_view()),
+    path('signup/', views.signup),
+    path('login/', views.login),
 ]
