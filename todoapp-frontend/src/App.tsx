694047f04ef21f8b074f0de9a4eb72613962f977
Danilo Packer <danilo.kpacker@gmail.com>
2022-08-29 23:13:53 +0000

    Creating login LoginModal

--- todoapp-frontend/src/App.tsx
+++ todoapp-frontend/src/App.tsx
@@ -2,16 +2,21 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import TodoList from './components/TodoList/TodoList';
 import { BrowserRouter } from 'react-router-dom';
+import { useState } from 'react';
 
 function App() {
 
+const [loginModalIsOpen, setLoginModalIsOpen] = useState(false);
 
   return (
     <BrowserRouter>
       <div className="App">
         <header className="App-header">
-          <Navbar/>
+          <Navbar setLoginModalIsOpen={setLoginModalIsOpen} />
         </header>
+        <div>
+          <p>{loginModalIsOpen}</p>
+        </div>
         <TodoList />
     </div>
     </BrowserRouter>
---
+++ todoapp-frontend/src/components/Login/LoginModal.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import style from '../Navbar/Navbar.module.scss'
+import { Route, Link } from 'react-router-dom';
+
+//interface Props {
+//    type?: "button" | "submit" | "reset" | undefined, 
+//} 
+
+
+function LoginModal (){
+
+}
+
+export default LoginModal
+
+
+
